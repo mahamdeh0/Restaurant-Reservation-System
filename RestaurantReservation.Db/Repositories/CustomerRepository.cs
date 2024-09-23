9cf2f5dcc39a4082ffe29890418d7396c550be0e
mahamdeh0 <mahamdehabd@gmail.com>
2024-09-23 18:10:48 +0300

    Refactor CustomerRepository: Remove IRepository<Customer> implementation

--- RestaurantReservation.Db/Repositories/CustomerRepository.cs
+++ RestaurantReservation.Db/Repositories/CustomerRepository.cs
@@ -3,7 +3,7 @@ using RestaurantReservation.Db.Models.Entities;
 
 namespace RestaurantReservation.Db.Repositories
 {
-    public class CustomerRepository : Repository<Customer>, IRepository<Customer>
+    public class CustomerRepository : Repository<Customer>
     {
         public CustomerRepository(RestaurantReservationDbContext context) : base(context) { }
 
