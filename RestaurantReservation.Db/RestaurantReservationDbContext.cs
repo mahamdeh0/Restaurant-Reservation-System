03a4b56a8c0c2d29933807b5ffb59e97f849f564
mahamdeh0 <mahamdehabd@gmail.com>
2024-09-23 11:29:39 +0300

    Added Configured DbContext and AppSettings.json for connection string

--- RestaurantReservation.Db/RestaurantReservationDbContext.cs
+++ RestaurantReservation.Db/RestaurantReservationDbContext.cs
@@ -4,6 +4,9 @@ namespace RestaurantReservation.Db
 {
     public class RestaurantReservationDbContext : DbContext
     {
+        public RestaurantReservationDbContext(DbContextOptions options) : base(options)
+        {
+        }
 
     }
 }
--- RestaurantReservation/Program.cs
+++ RestaurantReservation/Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using RestaurantReservation.Db;
+
+namespace RestaurantReservation
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder().AddJsonFile("AppSettings.json")
+                                                   .Build();
+
+            var connectionString = config.GetSection("constr").Value;
+            var optionsBuilder = new DbContextOptionsBuilder();
+            optionsBuilder.UseSqlServer(connectionString);
+            var options = optionsBuilder.Options;
+
+            using (var context = new RestaurantReservationDbContext(options))
+            {
+                //Test Connection
+
+            }
+
+
+        }
+    }
+}
