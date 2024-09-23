723c9a8656412268c04a5cb3cda82d0d3353b477
mahamdeh0 <mahamdehabd@gmail.com>
2024-09-23 17:45:56 +0300

    Implement generic repository pattern for data access in RestaurantReservation.Db

---
+++ RestaurantReservation.Db/Interfaces/IRepository.cs
@@ -0,0 +1,10 @@
+﻿namespace RestaurantReservation.Db.Interfaces
+{
+    public interface IRepository<TEntity>
+    {
+        public Task<TEntity> CreateAsync(TEntity entity);
+        public Task UpdateAsync(TEntity entity);
+        public Task DeleteAsync(int id);
+        public Task<TEntity> GetByIdAsync(int id);
+    }
+}
---
+++ RestaurantReservation.Db/Repositories/Repository.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RestaurantReservation.Db.Interfaces;
+
+namespace RestaurantReservation.Db.Repositories
+{
+    public class Repository<TEntity> : IRepository<TEntity> where TEntity : class
+    {
+        private readonly RestaurantReservationDbContext _context;
+        private readonly DbSet<TEntity> _dbSet;
+
+        public Repository(RestaurantReservationDbContext context)
+        {
+            _context = context;
+            _dbSet = context.Set<TEntity>();
+        }
+
+        public async Task<TEntity> CreateAsync(TEntity entity)
+        {
+            _dbSet.Add(entity);
+            await _context.SaveChangesAsync();
+            return entity;
+        }
+
+        public async Task UpdateAsync(TEntity entity)
+        {
+            _dbSet.Update(entity);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var entity = await GetByIdAsync(id);
+            if (entity != null)
+            {
+                _dbSet.Remove(entity);
+                await _context.SaveChangesAsync();
+            }
+        }
+
+        public async Task<TEntity> GetByIdAsync(int id)
+        {
+            return await _dbSet.FindAsync(id);
+        }
+    }
+}
