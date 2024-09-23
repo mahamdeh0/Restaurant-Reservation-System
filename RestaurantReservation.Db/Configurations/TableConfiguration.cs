74b335cf1f409cccfe706c611ad03165940e29b1
mahamdeh0 <mahamdehabd@gmail.com>
2024-09-23 13:40:04 +0300

    Added TableConfiguration class for entity configuration in RestaurantReservation.Db

---
+++ RestaurantReservation.Db/Configurations/TableConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using RestaurantReservation.Db.Models.Entities;
+
+namespace RestaurantReservation.Db.Configurations
+{
+    public class TableConfiguration : IEntityTypeConfiguration<Table>
+    {
+        public void Configure(EntityTypeBuilder<Table> builder)
+        {
+            builder.Property(t => t.TableId).HasColumnName("table_id");
+            builder.Property(t => t.RestaurantId).HasColumnName("restaurant_id");
+            builder.Property(t => t.Capacity).HasColumnName("capacity").IsRequired();
+
+            builder.HasOne(r => r.Restaurant)
+              .WithMany(t => t.Tables)
+              .HasForeignKey(r => r.RestaurantId)
+              .OnDelete(DeleteBehavior.ClientSetNull);
+        }
+    }
+}
