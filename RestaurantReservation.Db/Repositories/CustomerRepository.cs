eafaccb00962e08bb5ae3c0062cd93131fc797ba
mahamdeh0 <mahamdehabd@gmail.com>
2024-09-23 18:08:21 +0300

    Add CustomerRepository for CRUD operations and implement customer management in Program

---
+++ RestaurantReservation.Db/Repositories/CustomerRepository.cs
@@ -0,0 +1,11 @@
+﻿using RestaurantReservation.Db.Interfaces;
+using RestaurantReservation.Db.Models.Entities;
+
+namespace RestaurantReservation.Db.Repositories
+{
+    public class CustomerRepository : Repository<Customer>, IRepository<Customer>
+    {
+        public CustomerRepository(RestaurantReservationDbContext context) : base(context) { }
+
+    }
+}
--- RestaurantReservation/Program.cs
+++ RestaurantReservation/Program.cs
@@ -1,15 +1,29 @@
 ﻿using RestaurantReservation.Db;
+using RestaurantReservation.Db.Models.Entities;
+using RestaurantReservation.Db.Repositories;
 
 namespace RestaurantReservation
 {
-    public class Program
+    class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
+            var context = new RestaurantReservationDbContext(); 
+            var customerRepo = new CustomerRepository(context);
 
+            var newCustomer = new Customer { FirstName = "Hiba", LastName = "Kurd", Email = "Hiba@Foothill.com", PhoneNumber = "1234567890" };
+            await customerRepo.CreateAsync(newCustomer);
+            Console.WriteLine("Customer created!");
 
-            var context = new RestaurantReservationDbContext();
+            newCustomer.LastName = "AlKurd";
+            await customerRepo.UpdateAsync(newCustomer);
+            Console.WriteLine("Customer updated!");
 
+            var retrievedCustomer = await customerRepo.GetByIdAsync(newCustomer.CustomerId);
+            Console.WriteLine($"Retrieved Customer: {retrievedCustomer.FirstName} {retrievedCustomer.LastName}");
+
+            await customerRepo.DeleteAsync(newCustomer.CustomerId);
+            Console.WriteLine("Customer deleted!");
         }
     }
 }
