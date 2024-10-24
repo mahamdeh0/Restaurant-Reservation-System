ef51d51583a70275bd8ea30082210e31ca0f5479
mahamdeh0 <mahamdehabd@gmail.com>
2024-10-24 13:44:36 +0300

    Add AutoMapper profile for Customer mapping

---
+++ RestaurantReservation.API/Profiles/CustomerProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using RestaurantReservation.API.Models.Customers;
+using RestaurantReservation.Db.Models.Entities;
+
+namespace RestaurantReservation.API.Profiles
+{
+    public class CustomerProfile : Profile
+    {
+        public CustomerProfile()
+        {
+            CreateMap<Customer,CustomerDto>();
+        }
+    }
+}
