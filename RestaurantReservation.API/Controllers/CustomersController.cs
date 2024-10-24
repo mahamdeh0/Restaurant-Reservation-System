40a3c1a6af69f40b9e64c63147c7ce6ceda10bdb
mahamdeh0 <mahamdehabd@gmail.com>
2024-10-24 13:57:27 +0300

    Add CustomersController to manage customer data with GET endpoint for retrieving all customers

---
+++ RestaurantReservation.API/Controllers/CustomersController.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using RestaurantReservation.API.Models.Customers;
+using RestaurantReservation.Db.Interfaces;
+
+namespace RestaurantReservation.API.Controllers
+{
+    [Route("api/Customers")]
+    [ApiController]
+    public class CustomersController : ControllerBase
+    {
+        private readonly ICustomerRepository _customerRepository;
+        private readonly IMapper _mapper;
+
+        public CustomersController(ICustomerRepository customerRepository, IMapper Mapper)
+        {
+            _customerRepository = customerRepository;
+            _mapper = Mapper;
+        }
+
+        /// <summary>
+        /// Gets all customers.
+        /// </summary>
+        /// <returns>A list of customer DTOs.</returns>
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public async Task<ActionResult<IEnumerable<CustomerDto>>> GetAllCustomers()
+        {
+            var customers = await _customerRepository.GetAllAsync();
+
+            if (customers == null || !customers.Any())
+            {
+                return NoContent();
+            }
+
+            return Ok(_mapper.Map<IEnumerable<CustomerDto>>(customers));
+        }
+
+
+    }
+}
--- RestaurantReservation.API/Models/Customers/CustomerDto.cs
+++ RestaurantReservation.API/Models/Customers/CustomerDto.cs
@@ -5,6 +5,6 @@
         public int CustomerId { get; set; }
         public string FirstName { get; set; } = string.Empty;
         public string LastName { get; set; } = string.Empty;
-        public string? Phone {  get; set; }
+        public string? PhoneNumber {  get; set; }
     }   
 }
