4aadaf35a3df8b60f9e10765e47a9b01d73ce059
mahamdeh0 <mahamdehabd@gmail.com>
2024-10-25 17:57:29 +0300

    Add MenuItemProfile for mapping between MenuItem and MenuItemDto in AutoMapper configuration

---
+++ RestaurantReservation.API/Profiles/MenuItemProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using RestaurantReservation.API.Models.MenuItems;
+using RestaurantReservation.Db.Models.Entities;
+
+namespace RestaurantReservation.API.Profiles
+{
+    public class MenuItemProfile : Profile
+    {
+        public MenuItemProfile()
+        {
+            CreateMap<MenuItem, MenuItemDto>();
+
+        }
+    }
+}
