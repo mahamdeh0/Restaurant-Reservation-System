7bcf568e3b8eb7fbdf4edf381e687d889ea78686
mahamdeh0 <mahamdehabd@gmail.com>
2024-10-25 16:13:51 +0300

    Add EmployeeDto model with properties for employee details and position representation

---
+++ RestaurantReservation.API/Models/Employees/EmployeeDto.cs
@@ -0,0 +1,16 @@
+﻿using RestaurantReservation.Db.Models.Enum;
+
+namespace RestaurantReservation.API.Models.Employees
+{
+    public class EmployeeDto
+    {
+        public int EmployeeId { get; set; }
+        public int RestaurantId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public EmployeePosition Position { get; set; }
+        public string PositionName => ((EmployeePosition)Position).ToString();
+
+
+    }
+}
