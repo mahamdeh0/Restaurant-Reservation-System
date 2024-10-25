e8a3dabfa1e0375f3e2a4ef1e5ddeef437667693
mahamdeh0 <mahamdehabd@gmail.com>
2024-10-25 16:15:00 +0300

    Add EmployeesController with GetEmployees endpoint to retrieve all employees and return EmployeeDto list

---
+++ RestaurantReservation.API/Controllers/EmployeesController.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using RestaurantReservation.API.Models.Employees;
+using RestaurantReservation.Db.Interfaces;
+using RestaurantReservation.Db.Models.Entities;
+
+namespace RestaurantReservation.API.Controllers
+{
+    [Route("api/employees")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly IEmployeeRepository _employeeRepository;
+        private readonly IMapper _mapper;
+
+        public EmployeesController(IEmployeeRepository employeeRepository, IMapper mapper)
+        {
+            _employeeRepository = employeeRepository;
+            _mapper = mapper;
+        }
+
+        /// <summary>
+        /// Retrieves all employees.
+        /// </summary>
+        /// <returns>A list of employee DTOs.</returns>
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public async Task<ActionResult<IEnumerable<EmployeeDto>>> GetEmployees()
+        {
+            var allEmployees = await _employeeRepository.GetAllAsync();
+
+            if (allEmployees == null || !allEmployees.Any())
+            {
+                return NoContent();
+            }
+
+            return Ok(_mapper.Map<IEnumerable<EmployeeDto>>(allEmployees));
+        }
+
+    }
+}
