1a37d37d41a92b01aca6f93e706478877e1369b3
mahamdeh0 <mahamdehabd@gmail.com>
2024-10-25 19:36:08 +0300

    Add OrderItemDto model for order item data representation

---
+++ RestaurantReservation.API/Models/OrderItems/OrderItemDto.cs
@@ -0,0 +1,12 @@
+﻿using RestaurantReservation.Db.Models.Entities;
+
+namespace RestaurantReservation.API.Models.OrderItems
+{
+    public class OrderItemDto
+    {
+        public int OrderItemId { get; set; }
+        public int OrderId { get; set; }
+        public int ItemId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
