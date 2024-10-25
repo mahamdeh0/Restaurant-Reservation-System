709c81d387bb0349cd4d9f0ef496bdd168db5dc5
mahamdeh0 <mahamdehabd@gmail.com>
2024-10-25 16:14:25 +0300

    Add EmployeeProfile for mapping between Employee and EmployeeDto with custom mapping for PositionName

---
+++ RestaurantReservation.API/Profiles/EmployeeProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using RestaurantReservation.API.Models.Employees;
+using RestaurantReservation.Db.Models.Entities;
+using RestaurantReservation.Db.Models.Enum;
+
+namespace RestaurantReservation.API.Profiles
+{
+    public class EmployeeProfile : Profile
+    {
+        public EmployeeProfile()
+        {
+            CreateMap<Employee, EmployeeDto>().ForMember(dest => dest.PositionName, opt => opt.MapFrom(src => ((EmployeePosition)src.Position).ToString())); ;
+
+        }
+    }
+}
