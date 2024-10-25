d2fa5a73f2fcd4e777f7e6789a418fc1fb2a1047
mahamdeh0 <mahamdehabd@gmail.com>
2024-10-25 15:10:35 +0300

    Add CustomerUpdateDto model with properties for updating customer information

---
+++ RestaurantReservation.API/Models/Customers/CustomerUpdateDto.cs
@@ -0,0 +1,10 @@
+﻿namespace RestaurantReservation.API.Models.Customers
+{
+    public class CustomerUpdateDto
+    {
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Email { get; set; }
+        public string? PhoneNumber { get; set; }
+    }
+}
