eb3ea52b2fe68fc3bebf7e2f4732cf9e69e35def
mahamdeh0 <mahamdehabd@gmail.com>
2024-10-25 18:19:37 +0300

    Add MenuItemCreationDto model with properties for creating a new menu item

---
+++ RestaurantReservation.API/Models/MenuItems/MenuItemCreationDto.cs
@@ -0,0 +1,10 @@
+﻿namespace RestaurantReservation.API.Models.MenuItems
+{
+    public class MenuItemCreationDto
+    {
+        public int RestaurantId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+    }
+}
