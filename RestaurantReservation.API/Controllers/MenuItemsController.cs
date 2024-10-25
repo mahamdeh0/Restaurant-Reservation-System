91844df16b7ea1629ce4cff377052b9a8d9a8a87
mahamdeh0 <mahamdehabd@gmail.com>
2024-10-25 17:59:25 +0300

    Add MenuItemsController with GetMenuItems endpoint to retrieve all menu items

---
+++ RestaurantReservation.API/Controllers/MenuItemsController.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using RestaurantReservation.API.Models.MenuItems;
+using RestaurantReservation.Db.Interfaces;
+
+namespace RestaurantReservation.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MenuItemsController : ControllerBase
+    {
+        private readonly IMenuItemRepository _menuItemRepository;
+        private readonly IMapper _mapper;
+
+        public MenuItemsController(IMenuItemRepository menuItemRepository, IMapper mapper)
+        {
+            _menuItemRepository = menuItemRepository;
+            _mapper = mapper;
+        }
+
+        /// <summary>
+        /// Retrieves all menu items.
+        /// </summary>
+        /// <returns>A list of menu item DTOs if available; otherwise, a 204 No Content response if no menu items exist.</returns>
+        [HttpGet]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        public async Task<ActionResult<IEnumerable<MenuItemDto>>> GetMenuItems()
+        {
+            var menuItems = await _menuItemRepository.GetAllAsync();
+
+            if (!menuItems.Any())
+            {
+                return NoContent();
+            }
+
+            return Ok(_mapper.Map<IEnumerable<MenuItemDto>>(menuItems));
+        }
+
+
+    }
+}
