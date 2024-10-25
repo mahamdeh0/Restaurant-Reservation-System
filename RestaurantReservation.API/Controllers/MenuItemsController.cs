1cb92cf20f184dd3f2948ec3745cf90219ce1b6c
mahamdeh0 <mahamdehabd@gmail.com>
2024-10-25 18:20:00 +0300

    Add CreateMenuItem endpoint to create a new menu item with validation for restaurant existence

--- RestaurantReservation.API/Controllers/MenuItemsController.cs
+++ RestaurantReservation.API/Controllers/MenuItemsController.cs
@@ -2,21 +2,26 @@
 using Microsoft.AspNetCore.Mvc;
 using RestaurantReservation.API.Models.MenuItems;
 using RestaurantReservation.Db.Interfaces;
+using RestaurantReservation.Db.Models.Entities;
 
 namespace RestaurantReservation.API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("api/menu-items")]
     [ApiController]
     public class MenuItemsController : ControllerBase
     {
         private readonly IMenuItemRepository _menuItemRepository;
+        private readonly IRestaurantRepository _restaurantRepository;
         private readonly IMapper _mapper;
 
-        public MenuItemsController(IMenuItemRepository menuItemRepository, IMapper mapper)
+        public MenuItemsController(IMenuItemRepository menuItemRepository, IRestaurantRepository restaurantRepository, IMapper mapper)
         {
             _menuItemRepository = menuItemRepository;
+            _restaurantRepository = restaurantRepository;
             _mapper = mapper;
         }
+
+
 
         /// <summary>
         /// Retrieves all menu items.
@@ -35,6 +40,28 @@ namespace RestaurantReservation.API.Controllers
             }
 
             return Ok(_mapper.Map<IEnumerable<MenuItemDto>>(menuItems));
+        }
+
+        /// <summary>
+        /// Creates a new menu item.
+        /// </summary>
+        /// <param name="menuItemCreationDto">The menu item creation data.</param>
+        /// <returns>A 201 Created response with the created menu item DTO if successful; otherwise, a 404 Not Found response if the restaurant does not exist.</returns>
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<ActionResult<MenuItemDto>> CreateMenuItem(MenuItemCreationDto menuItemCreationDto)
+        {
+            if (!await _restaurantRepository.RestaurantExistsAsync(menuItemCreationDto.RestaurantId))
+            {
+                return NotFound(new { Message = "Restaurant not found." });
+            }
+
+            var menuItemToAdd = _mapper.Map<MenuItem>(menuItemCreationDto);
+            var addedMenuItem = await _menuItemRepository.CreateAsync(menuItemToAdd);
+            var createdMenuItemDto = _mapper.Map<MenuItemDto>(addedMenuItem);
+
+            return Ok(createdMenuItemDto);
         }
 
 
