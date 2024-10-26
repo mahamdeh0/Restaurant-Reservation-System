8ed17c88a5941210b8c138ebf6ec347be3353391
mahamdeh0 <mahamdehabd@gmail.com>
2024-10-26 11:18:09 +0300

    Add OrderCreationDto class for order creation data transfer

---
+++ RestaurantReservation.API/Models/Orders/OrderCreationDto.cs
@@ -0,0 +1,10 @@
+﻿namespace RestaurantReservation.API.Models.Orders
+{
+    public class OrderCreationDto
+    {
+        public int ReservationId { get; set; }
+        public int EmployeeId { get; set; }
+        public DateTime OrderDate { get; set; }
+        public int TotalAmount { get; set; }
+    }
+}
