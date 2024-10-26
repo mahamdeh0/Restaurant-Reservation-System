92aeb62f78b5e4771f45b97a993b80864d7ee949
mahamdeh0 <mahamdehabd@gmail.com>
2024-10-26 10:49:17 +0300

    Implement update and partial update methods for order items

--- RestaurantReservation.API/Controllers/OrderItemsController.cs
+++ RestaurantReservation.API/Controllers/OrderItemsController.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using Microsoft.AspNetCore.JsonPatch;
 using Microsoft.AspNetCore.Mvc;
 using RestaurantReservation.API.Models.OrderItems;
 using RestaurantReservation.Db.Interfaces;
@@ -62,5 +63,74 @@ namespace RestaurantReservation.API.Controllers
 
             return Ok(_mapper.Map<OrderItemDto>(orderItem));
         }
+
+        /// <summary>
+        /// Updates an existing order item within a specified order.
+        /// </summary>
+        /// <param name="orderId">The ID of the order that contains the order item.</param>
+        /// <param name="id">The ID of the order item to update.</param>
+        /// <param name="orderItemUpdateDto">The updated data for the order item.</param>
+        /// <returns>A 204 No Content response if successful; otherwise, a 404 Not Found response if the order or order item does not exist, or if the order item does not belong to the specified order.</returns>
+        [HttpPut]
+        public async Task<IActionResult> UpdateOrderItem(int orderId, int id, OrderItemUpdateDto orderItemUpdateDto)
+        {
+            if (!await _orderRepository.OrderItemExistsAsync(orderId))
+                return NotFound("No order found with the specified ID.");
+
+
+            var orderItem = await _orderItemRepository.GetByIdAsync(id);
+
+            if (orderItem is null)
+                return NotFound("Order item with the given ID is not found.");
+
+            if (orderItem.OrderId != orderId)
+                return NotFound("The specified order does not contain an item with the given ID.");
+
+            _mapper.Map(orderItemUpdateDto, orderItem);
+
+            await _orderItemRepository.UpdateAsync(orderItem);
+
+            return NoContent();
+        }
+
+        /// <summary>
+        /// Partially updates an existing order item within a specified order using a JSON patch document.
+        /// </summary>
+        /// <param name="orderId">The ID of the order that contains the order item.</param>
+        /// <param name="id">The ID of the order item to partially update.</param>
+        /// <param name="patchDocument">The JSON patch document containing the updates to apply.</param>
+        /// <returns>A 204 No Content response if successful; otherwise, a 404 Not Found response if the order or order item does not exist, or if the order item does not belong to the specified order, or a 400 Bad Request if the model state is invalid.</returns>
+        [HttpPatch]
+        public async Task<IActionResult> PartiallyUpdateOrderItem(int orderId, int id, JsonPatchDocument<OrderItemUpdateDto> patchDocument)
+        {
+            if (!await _orderRepository.OrderItemExistsAsync(orderId))
+                return NotFound("No order found with the specified ID.");
+
+            var orderItem = await _orderItemRepository.GetByIdAsync(id);
+
+            if (orderItem is null)
+                return NotFound("Order item with the given ID is not found.");
+
+            if (orderItem.OrderId != orderId)
+                return NotFound("The specified order does not contain an item with the given ID.");
+
+            var orderItemToPatch = _mapper.Map<OrderItemUpdateDto>(orderItem);
+
+            patchDocument.ApplyTo(orderItemToPatch, ModelState);
+
+            if (!ModelState.IsValid || !TryValidateModel(orderItemToPatch))
+            {
+                return BadRequest(ModelState);
+            }
+
+            _mapper.Map(orderItemToPatch, orderItem);
+
+            await _orderItemRepository.UpdateAsync(orderItem);
+
+            return NoContent();
+        }
+
+
+
     }
 }
--- RestaurantReservation.API/Models/OrderItems/OrderItemDto.cs
+++ RestaurantReservation.API/Models/OrderItems/OrderItemDto.cs
@@ -1,10 +1,7 @@
-﻿using RestaurantReservation.Db.Models.Entities;
-
-namespace RestaurantReservation.API.Models.OrderItems
+﻿namespace RestaurantReservation.API.Models.OrderItems
 {
     public class OrderItemDto
     {
-        public int OrderItemId { get; set; }
         public int OrderId { get; set; }
         public int ItemId { get; set; }
         public int Quantity { get; set; }
