a99395c5e1ea2cb407a9c1e71b407b08d0eac334
mahamdeh0 <mahamdehabd@gmail.com>
2024-10-27 16:21:52 +0200

    Implement FluentValidation for OrderItemCreationDto and OrderItemUpdateDto validators

---
+++ RestaurantReservation.API/Validators/OrderItems/OrderItemUpdateValidator.cs
@@ -0,0 +1,14 @@
+﻿using FluentValidation;
+using RestaurantReservation.API.Models.OrderItems;
+
+namespace RestaurantReservation.API.Validators.OrderItems
+{
+    public class OrderItemUpdateValidator : AbstractValidator<OrderItemUpdateDto>
+    {
+        public OrderItemUpdateValidator()
+        {
+            RuleFor(x => x.Quantity)
+                .GreaterThan(0).WithMessage("Quantity must be greater than zero.");
+        }
+    }
+}
