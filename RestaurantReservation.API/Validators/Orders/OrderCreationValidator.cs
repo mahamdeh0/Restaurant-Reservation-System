fd44516327cfedea058c1c19d9de35be08f989c0
mahamdeh0 <mahamdehabd@gmail.com>
2024-10-27 16:27:45 +0200

    Implement FluentValidation for OrderCreationDto and OrderUpdateDto validators

---
+++ RestaurantReservation.API/Validators/Orders/OrderCreationValidator.cs
@@ -0,0 +1,23 @@
+﻿using FluentValidation;
+using RestaurantReservation.API.Models.Orders;
+
+namespace RestaurantReservation.API.Validators.Orders
+{
+    public class OrderCreationValidator : AbstractValidator<OrderCreationDto>
+    {
+        public OrderCreationValidator()
+        {
+            RuleFor(x => x.ReservationId)
+                .NotEmpty().WithMessage("Reservation ID is required.");
+
+            RuleFor(x => x.EmployeeId)
+                .NotEmpty().WithMessage("Employee ID is required."); 
+
+            RuleFor(x => x.OrderDate)
+                .GreaterThan(DateTime.Now).WithMessage("Order date must be in the future.");
+
+            RuleFor(x => x.TotalAmount)
+                .GreaterThan(0).WithMessage("Total amount must be greater than zero.");
+        }
+    }
+}
---
+++ RestaurantReservation.API/Validators/Orders/OrderUpdateValidator.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using RestaurantReservation.API.Models.Orders;
+
+namespace RestaurantReservation.API.Validators.Orders
+{
+    public class OrderUpdateValidator : AbstractValidator<OrderUpdateDto>
+    {
+        public OrderUpdateValidator()
+        {
+            RuleFor(x => x.ReservationId)
+                .NotEmpty().WithMessage("Reservation ID is required."); 
+
+            RuleFor(x => x.EmployeeId)
+                .NotEmpty().WithMessage("Employee ID is required."); 
+
+            RuleFor(x => x.TotalAmount)
+                .GreaterThan(0).WithMessage("Total amount must be greater than zero.");
+        }
+    }
+}
