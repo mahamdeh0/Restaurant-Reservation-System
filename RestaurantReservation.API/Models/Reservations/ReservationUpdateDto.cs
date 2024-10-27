1e869ba81b43437d93f097ad9ca9a4fb8497fe26
mahamdeh0 <mahamdehabd@gmail.com>
2024-10-27 10:22:01 +0200

    Add ReservationCreationDto and ReservationUpdateDto classes for reservation data transfer

---
+++ RestaurantReservation.API/Models/Reservations/ReservationUpdateDto.cs
@@ -0,0 +1,10 @@
+﻿namespace RestaurantReservation.API.Models.Reservations
+{
+    public class ReservationUpdateDto
+    {
+        public int CustomerId { get; set; }
+        public int RestaurantId { get; set; }
+        public int TableId { get; set; }
+        public int PartySize { get; set; }
+    }
+}
