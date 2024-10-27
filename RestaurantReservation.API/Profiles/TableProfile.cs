8759da7b6b054fb9cfc5368a9d8ed2d3fa1bb8bc
mahamdeh0 <mahamdehabd@gmail.com>
2024-10-27 11:09:46 +0200

    Configure AutoMapper profiles for table DTOs and entities

---
+++ RestaurantReservation.API/Profiles/TableProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using RestaurantReservation.API.Models.Tables;
+using RestaurantReservation.Db.Models.Entities;
+
+namespace RestaurantReservation.API.Profiles
+{
+    public class TableProfile : Profile
+    {
+        public TableProfile()
+        {
+            CreateMap<Table, TableDto>();
+            CreateMap<TableCreationDto, Table>();
+            CreateMap<TableUpdateDto, Table>().ReverseMap();
+        }
+    }
+}
