1218224952eff39812514a716ff7c9af81caa8b7
mahamdeh0 <mahamdehabd@gmail.com>
2024-10-27 12:33:00 +0200

    Add CustomerExistsAsync method to ICustomerRepository interface

--- RestaurantReservation.Db/Interfaces/ICustomerRepository.cs
+++ RestaurantReservation.Db/Interfaces/ICustomerRepository.cs
@@ -5,6 +5,7 @@ namespace RestaurantReservation.Db.Interfaces
     public interface ICustomerRepository : IRepository<Customer>
     {
         Task<List<Customer>> GetCustomersWithReservationsAbovePartySizeAsync(int PartySize);
+        public Task<bool> CustomerExistsAsync(int customerId);
 
     }
 }
