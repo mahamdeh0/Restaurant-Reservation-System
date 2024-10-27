b28a22325094a55b8e9c556b7399323d8bbbc2a7
mahamdeh0 <mahamdehabd@gmail.com>
2024-10-27 16:30:51 +0200

    Implement FluentValidation for TableCreationDto and TableUpdateDto validators

---
+++ RestaurantReservation.API/Validators/Tables/TableUpdateValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using RestaurantReservation.API.Models.Tables;
+
+namespace RestaurantReservation.API.Validators.Tables
+{
+    public class TableUpdateValidator : AbstractValidator<TableUpdateDto>
+    {
+        public TableUpdateValidator()
+        {
+            RuleFor(x => x.RestaurantId)
+                .NotEmpty().WithMessage("Restaurant ID is required.")
+
+            RuleFor(x => x.Capacity)
+                .NotEmpty().WithMessage("Capacity is required.")
+                .GreaterThan(0).WithMessage("Capacity must be greater than zero.");
+        }
+    }
+}
