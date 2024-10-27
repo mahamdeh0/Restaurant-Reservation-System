dd6b93156e960cdbef4c20291c67198a52ec33a4
mahamdeh0 <mahamdehabd@gmail.com>
2024-10-27 09:49:44 +0200

    Add MenuItemExistsAsync method to IMenuItemRepository and implement in MenuItemRepository

--- RestaurantReservation.Db/Interfaces/IMenuItemRepository.cs
+++ RestaurantReservation.Db/Interfaces/IMenuItemRepository.cs
@@ -5,6 +5,8 @@ namespace RestaurantReservation.Db.Interfaces
     public interface IMenuItemRepository : IRepository<MenuItem>
     {
         public Task<List<MenuItem>> ListOrderedMenuItemsAsync(int reservationId);
+        public Task<bool> MenuItemExistsAsync(int itemId);
+
 
     }
 }
