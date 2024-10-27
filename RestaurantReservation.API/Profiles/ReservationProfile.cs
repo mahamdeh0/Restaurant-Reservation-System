d1ca35abf2ac43ac9553bea930db4741d901b845
mahamdeh0 <mahamdehabd@gmail.com>
2024-10-27 10:16:24 +0200

    Add ReservationProfile for mapping between Reservation and ReservationDto with AutoMapper

---
+++ RestaurantReservation.API/Profiles/ReservationProfile.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using RestaurantReservation.API.Models.Reservations;
+using RestaurantReservation.Db.Models.Entities;
+
+namespace RestaurantReservation.API.Profiles
+{
+    public class ReservationProfile : Profile
+    {
+        public ReservationProfile()
+        {
+            CreateMap<Reservation, ReservationDto>();
+
+        }
+    }
+}
