dd31087fe0727f560bf6944f6e7d331c71a9c9b7
mahamdeh0 <mahamdehabd@gmail.com>
2024-10-27 09:54:23 +0200

    Add OrderUpdateDto class for updating order details

---
+++ RestaurantReservation.API/Models/Orders/OrderUpdateDto.cs
@@ -0,0 +1,9 @@
+﻿namespace RestaurantReservation.API.Models.Orders
+{
+    public class OrderUpdateDto
+    {
+        public int ReservationId { get; set; }
+        public int EmployeeId { get; set; }
+        public int TotalAmount { get; set; }
+    }
+}
