74d771c05e3bde6d46d39cae6574c6f7cb8d7a40
mahamdeh0 <mahamdehabd@gmail.com>
2024-10-27 16:25:49 +0200

    Add FluentValidation for ReservationCreationDto and ReservationUpdateDto validators

---
+++ RestaurantReservation.API/Validators/Reservations/ReservationCreationValidator.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using RestaurantReservation.API.Models.Reservations;
+
+namespace RestaurantReservation.API.Validators.Reservations
+{
+    public class ReservationCreationValidator : AbstractValidator<ReservationCreationDto>
+    {
+        public ReservationCreationValidator()
+        {
+            RuleFor(x => x.CustomerId)
+                .NotEmpty().WithMessage("Customer ID is required."); 
+
+            RuleFor(x => x.RestaurantId)
+                .NotEmpty().WithMessage("Restaurant ID is required.");
+
+            RuleFor(x => x.TableId)
+                .NotEmpty().WithMessage("Table ID is required."); 
+
+            RuleFor(x => x.ReservationDate)
+                .GreaterThan(DateTime.Now).WithMessage("Reservation date must be in the future.");
+
+            RuleFor(x => x.PartySize)
+                .GreaterThan(0).WithMessage("Party size must be greater than zero.");
+        }
+    }
+}
