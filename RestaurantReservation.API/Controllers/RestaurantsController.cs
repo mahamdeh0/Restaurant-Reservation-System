3423c0a9855cd130d37f74c7862430eb7f8b7c40
mahamdeh0 <mahamdehabd@gmail.com>
2024-10-27 10:48:08 +0200

    Implement RestaurantsController with GetRestaurants

---
+++ RestaurantReservation.API/Controllers/RestaurantsController.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using RestaurantReservation.API.Models.Restaurants;
+using RestaurantReservation.Db.Interfaces;
+
+namespace RestaurantReservation.API.Controllers
+{
+    [Route("api/restaurants")]
+    [ApiController]
+    public class RestaurantsController : ControllerBase
+    {
+        private readonly IRestaurantRepository _restaurantRepository;
+
+        private readonly IMapper _mapper;
+
+        public RestaurantsController(IRestaurantRepository restaurantRepository, IMapper mapper)
+        {
+            _restaurantRepository = restaurantRepository;
+            _mapper = mapper;
+        }
+
+
+        /// <summary>
+        /// Gets all restaurants.
+        /// </summary>
+        /// <returns>A list of RestaurantDto.</returns>
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<RestaurantDto>>> GetRestaurants()
+        {
+
+            var restaurants = await _restaurantRepository.GetAllAsync();
+
+            if (restaurants == null || !restaurants.Any())
+                return NotFound("No restaurants found.");
+
+            return Ok(_mapper.Map<IEnumerable<RestaurantDto>>(restaurants));
+        }
+
+    }
+}
