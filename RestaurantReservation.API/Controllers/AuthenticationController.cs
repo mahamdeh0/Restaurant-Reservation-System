6563b48e1716a7ad5a3eb6051bb00fe6cf4c4187
mahamdeh0 <mahamdehabd@gmail.com>
2024-11-14 17:49:39 +0200

    Simplify AuthenticationController constructor using .NET 8 single-line constructor syntax

--- RestaurantReservation.API/Controllers/AuthenticationController.cs
+++ RestaurantReservation.API/Controllers/AuthenticationController.cs
@@ -6,24 +6,15 @@ namespace RestaurantReservation.API.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class AuthenticationController : ControllerBase
+    public class AuthenticationController(IJwtTokenGenerator jwtTokenGenerator) : ControllerBase
     {
-        private readonly IJwtTokenGenerator _jwtTokenGenerator;
-
-        public AuthenticationController(IJwtTokenGenerator jwtTokenGenerator)
-        {
-            _jwtTokenGenerator = jwtTokenGenerator;
-        }
+        private readonly IJwtTokenGenerator _jwtTokenGenerator = jwtTokenGenerator;
 
         [HttpPost("login")]
         public IActionResult Login(string username, string password)
         {
-
             var token = _jwtTokenGenerator.GenerateToken(username, password);
             return Ok(new { Token = token });
-
         }
-
     }
 }
-
